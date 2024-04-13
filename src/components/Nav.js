1f1ac2756b50e25b6bb3923a3990fc82a751e765
William Hung <william851214@gmail.com>
2024-04-13 22:31:38 +1000

    set up navbar with bootstrap and sass

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,68 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../styles/style.css";
 
 const Nav = () => {
 	return (
 		<div>
-			<nav>
-				<ul>
-					<li>
-						<NavLink to="/">Home</NavLink>
-					</li>
-					<li>
-						<NavLink to="/about">About</NavLink>
-					</li>
-					<li>
-						<NavLink to="/resume">Resume</NavLink>
-					</li>
-					<li>
-						<NavLink to="/portfolio">Portfolio</NavLink>
-					</li>
-				</ul>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<div className="container-fluid">
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNav"
+						aria-controls="navbarNav"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse justify-content-center"
+						id="navbarNav"
+					>
+						<ul className="navbar-nav">
+							<li className="nav-item">
+								<NavLink
+									className="nav-link navlink-custom"
+									activeClassName="active"
+									exact
+									to="/"
+								>
+									Home
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="nav-link navlink-custom"
+									activeClassName="active"
+									to="/about"
+								>
+									About
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="nav-link navlink-custom"
+									activeClassName="active"
+									to="/resume"
+								>
+									Resume
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="nav-link navlink-custom"
+									activeClassName="active"
+									to="/portfolio"
+								>
+									Portfolio
+								</NavLink>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</nav>
 		</div>
 	);
