bf27790c17755a02189a927c0274d134edfa49df
William Hung <william851214@gmail.com>
2024-04-15 22:12:35 +1000

    Add resume info

--- src/data/info.js
+++ src/data/info.js
@@ -9,6 +9,83 @@ const INFO = {
 		description:
 			"A passionate student at QUT pursuing a Master of IT with a focus on computer science. With a strong interest on machine learning and front-end development, I experienced two months of valuable internship experience as a Front End Intern. I thrive both as a collaborative team member and as an independent worker. I am characterized by my rapid learning abilities, keen attention to detail, and an unwavering eagerness to acquire new skills and tackle challenges.",
 	},
+	resume: {
+		title: "Resume",
+		summary:
+			"A passionate student at QUT pursuing a Master of IT with a focus on computer science. With a strong interest on machine learning and front-end development, I experienced two months of valuable internship experience as a Front End Intern. I thrive both as a collaborative team member and as an independent worker. I am characterized by my rapid learning abilities, keen attention to detail, and an unwavering eagerness to acquire new skills and tackle challenges.",
+		career: [
+			{
+				title: "Software Developer Intern",
+				date: "Jan 2024 - present",
+				location: "(Remote) Taipei, Taiwan",
+				company: "E2NOVA",
+				companyLogo: "",
+				companyInfo:
+					"A cloud communication platform for business",
+				companyLink: "https://www.evoxglobal.com/en/home",
+				description: [
+					"Proficient in drafting comprehensive API documentation and conducting thorough testing procedures to ensure functionality, reliability, and security of APIs.",
+					"Skilled in developing sample code snippets in Golang and PHP to demonstrate API integration and usage, facilitating seamless integration for developers and clients.",
+					"Completed formal training in AWS Connect, gaining proficiency in leveraging AWS Connect services to build and manage scalable and secure contact center solutions.",
+				],
+			},
+			{
+				title: "Frontend Intern",
+				date: "Dec 2024 - Jan 2023",
+				location: "Taipei, Taiwan",
+				company: "Askplore",
+				companyLogo: "",
+				companyInfo:
+					'Specializes in "website design", "system development", "graphic design" and "brand planning" to help customers expand their business smoothly.',
+				companyLink: "",
+				description: [
+					"Used Vue.js to create a case page for company official website",
+					"Adjusted and updated website page under the direction of the supervisor",
+				],
+			},
+			{
+				title: "Creator Operation Manager",
+				date: "Mar 2021 - Apr 2022",
+				location: "Taipei, Taiwan",
+				company: "Encore Days",
+				companyLogo: "",
+				companyInfo:
+					"Social media platform for the pre-Millennial generations to increase their social capital, backed by Taiwan FamilyMart",
+				companyLink: "https://www.evoxglobal.com/en/home",
+				description: [
+					"Increased the number of KOC on Encore platform from 30 to 100 under a month",
+					"Increased 15% on Facebook engagement (Share, Like, Comment) through social meida  operation within a month",
+				],
+			},
+			{
+				title: "Venture Capital Intern",
+				date: "Aug 2018 - Sep 2018",
+				location: "Taipei, Taiwan",
+				company: "CSC Venture Capital",
+				companyLogo: "",
+				companyInfo: "",
+				companyLink: "https://www.evoxglobal.com/en/home",
+				description: [
+					"Delivered an Online Furniture industry analysis report and provided investment recommendations based on trends",
+					"Supported an Asia-based music education platform to raise their Series B fundraising round",
+				],
+			},
+		],
+		skills: {
+			programmingLanguage: ["JavaScript", "C#", "Python"],
+			framework: [
+				"React.js",
+				"Vue.js(learning)",
+				"Node.js",
+			],
+			database: ["MySQL", "MongoDB"],
+			versionControl: ["Git", "GitHub"],
+			pmTool: ["JIRA"],
+			cloudServices: ["AWS"],
+		},
+		outputs: {},
+		qualifications: {},
+	},
 };
 
 export default INFO;
