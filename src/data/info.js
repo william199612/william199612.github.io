db51a471a7406ebd841953bbe574f48886bbb76d
william199612 <william851214@gmail.com>
2024-04-15 21:38:05 +1000

    about page first draft (#2)
    * separate info file
    
    * import info in home page
    
    * rename info file
    
    * add about page first draft

---
+++ src/data/info.js
@@ -0,0 +1,14 @@
+const INFO = {
+	home: {
+		title: "Welcome",
+		description:
+			"A passionate student at QUT pursuing a Master of IT with a focus on computer science.",
+	},
+	about: {
+		title: "About Me",
+		description:
+			"A passionate student at QUT pursuing a Master of IT with a focus on computer science. With a strong interest on machine learning and front-end development, I experienced two months of valuable internship experience as a Front End Intern. I thrive both as a collaborative team member and as an independent worker. I am characterized by my rapid learning abilities, keen attention to detail, and an unwavering eagerness to acquire new skills and tackle challenges.",
+	},
+};
+
+export default INFO;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,36 @@
+import React from "react";
+
+import INFO from "../data/info";
+
 export default function About() {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
-    );
+	return (
+		<div className="page-container">
+			<div className="title-section">
+				<h1 className="content-title text-center title">
+					{INFO.about.title}
+				</h1>
+			</div>
+			<div className="content-section row">
+				<div className="col-md-6">
+					<div className="left-section">
+						<div className="round-img-container">
+							<img
+								className="round-image"
+								src="https://images.unsplash.com/photo-1595947006841-0984683d036e?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+								alt=""
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-6">
+					<div className="right-section">
+						<div className="secondary-title">Hi!</div>
+						<p className="secondary-description">
+							{INFO.about.description}
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
