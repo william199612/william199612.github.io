654b2c430ae99dd7f282b0d828d6a9ffa9032f78
William Hung <william851214@gmail.com>
2024-04-15 20:36:20 +1000

    separate info file

---
+++ src/data/user.js
@@ -0,0 +1,9 @@
+const INFO = {
+	home: {
+		title: "Welcome",
+		description:
+			"A passionate student at QUT pursuing a Master of IT with a focus on computer science.",
+	},
+};
+
+export default INFO;
