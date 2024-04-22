346f43edbfe10843c4670dbb90c9745b23cb2e7c
William Hung <william851214@gmail.com>
2024-04-22 22:06:28 +1000

    Add github token

--- src/services/githubAPI.js
+++ src/services/githubAPI.js
@@ -1,7 +1,14 @@
 export const fetchGitHubData = async (url) => {
-	let response = await fetch(url);
+	const token = process.env.REACT_APP_GITHUB_API_KEY;
+	let response = await fetch(url, {
+		headers: {
+			Accept: "application/vnd.github+json",
+			Authorization: `Bearer ${token}`,
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
+	});
 	if (!response.ok) {
-		throw new Error("Fail to fetch image");
+		throw new Error("Fail to fetch from GitHub...");
 	}
 	let data = await response.json();
 	// console.log(data);
