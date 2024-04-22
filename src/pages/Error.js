cdf882b2543e41cf1a72548332e2d5500aa66f20
William Hung <william851214@gmail.com>
2024-04-22 22:29:01 +1000

    Add Error page and layout

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import ErrorIcon from "@mui/icons-material/Error";
 
 const ErrorPage = () => {
-	return <div>404 Not Found</div>;
+	return (
+		<div className="error-page">
+			<div className="error-msg">
+				<Typography variant="h3">
+					<ErrorIcon />
+					404 Page Not Found
+				</Typography>
+				<br />
+				<Typography variant="h4">Oooooops...</Typography>
+				<Typography variant="h6">
+					Something went wrong.
+				</Typography>
+				<Typography variant="h6">
+					Please choose another page on the top navbar!
+				</Typography>
+			</div>
+		</div>
+	);
 };
 
 export default ErrorPage;
