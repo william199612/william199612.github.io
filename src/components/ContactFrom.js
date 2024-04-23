a996ed56d18b4da3132b7ad429f63121624ffb95
William Hung <william851214@gmail.com>
2024-04-23 22:28:14 +1000

    Implement Email sending service

--- src/components/ContactFrom.js
+++ src/components/ContactFrom.js
@@ -1,35 +1,95 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+import { Typography } from "@mui/material";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
+
+const EmailSuccessMessage = () => {
+	return (
+		<div className="success-message">
+			<Typography>
+				<DoneAllIcon /> Message Sent!
+			</Typography>
+		</div>
+	);
+};
 
 export default function ContactForm() {
+	const [sent, setSent] = useState(true);
+	const form = useRef();
+
+	const serviceId = process.env.REACT_APP_EMAIL_SERVICE_ID;
+	const templateId =
+		process.env.REACT_APP_EMAIL_TEMPLATE_ID;
+	const publicKey = process.env.REACT_APP_EMAIL_KEY;
+
+	const handleEmailSent = () => {
+		setTimeout(() => {
+			setSent(true);
+		}, 1000);
+
+		setTimeout(() => {
+			setSent(false);
+		}, 3000);
+	};
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(serviceId, templateId, form.current, {
+				publicKey: publicKey,
+			})
+			.then(
+				() => {
+					console.log("SUCCESS!");
+					e.target.reset();
+				},
+				(error) => {
+					console.log("FAILED...", error.text);
+				}
+			);
+	};
+
 	return (
 		<div className="card card-container">
-			<div className="card-body card-body-custom">
+			<form
+				className="card-body card-body-custom"
+				ref={form}
+				onSubmit={sendEmail}
+			>
 				<p className="card-title">Contact Me !</p>
 				<input
 					type="text"
 					className="input-layout"
+					name="user_name"
 					id="name"
 					placeholder="Name"
 				/>
 				<input
 					type="email"
 					className="input-layout"
+					name="user_email"
 					id="email"
 					placeholder="Email"
 				/>
 				<textarea
 					className="textarea-layout"
 					placeholder="What you want to say to me..."
+					name="message"
 					id="message"
 					rows="3"
 				></textarea>
 				<button
 					type="submit"
 					className="btn btn-primary submit-buttom"
+					value="Send"
+					onClick={handleEmailSent}
 				>
 					Submit
 				</button>
-			</div>
+				{sent && <EmailSuccessMessage />}
+			</form>
 		</div>
 	);
 }
